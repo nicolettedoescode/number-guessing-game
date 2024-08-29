985f8f7780f8e7738e334439e1a88a5dd2c1905a
Nicolette Jacqueline <nicolettejacqueline@Nicolettes-MacBook-Pro.local>
2024-08-29 03:23:52 +0100

    Describe your changes here

--- app.py
+++ app.py
@@ -1,20 +1,15 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, make_response
 import random
-import os
 
 app = Flask(__name__)
 
-# Function to get the high score from a file
-def get_high_score():
-    if os.path.exists("highscore.txt"):
-        with open("highscore.txt", "r") as file:
-            return int(file.read())
-    return None
+# Function to get high score from cookies
+def get_high_score(cookies):
+    return int(cookies.get('high_score', 0))
 
-# Function to save the high score to a file
-def save_high_score(score):
-    with open("highscore.txt", "w") as file:
-        file.write(str(score))
+# Function to save high score to cookies
+def save_high_score(response, score):
+    response.set_cookie('high_score', str(score))
 
 # Function to check for achievements
 def check_achievements(attempts, correct_guesses):
@@ -33,14 +28,13 @@ def check_achievements(attempts, correct_guesses):
 
 @app.route('/')
 def index():
-    high_score = get_high_score()
-    return render_template('index.html', high_score=high_score if high_score is not None else 0)
+    return render_template('index.html')
 
 @app.route('/guess', methods=['POST'])
 def guess():
     user_guess = int(request.form['guess'])
-    number = int(request.cookies.get('number'))
-    attempts = int(request.cookies.get('attempts')) + 1
+    number = int(request.cookies.get('number', random.randint(1, 100)))
+    attempts = int(request.cookies.get('attempts', '0')) + 1
     correct_guesses = int(request.cookies.get('correct_guesses', '0'))
 
     response = {
@@ -55,17 +49,18 @@ def guess():
         response['achievements'] = check_achievements(attempts, correct_guesses)
 
         # Check and update high score
-        high_score = get_high_score()
+        high_score = get_high_score(request.cookies)
         if high_score is None or attempts < high_score:
-            save_high_score(attempts)
+            high_score = attempts
             response['message'] += ' 🎉 New high score!'
+            response['new_high_score'] = high_score
 
     else:
         response['message'] = 'Too high! Try again.' if user_guess > number else 'Too low! Try again.'
         response['status'] = 'wrong'
         correct_guesses = 0  # Reset correct guesses on a wrong attempt
 
-    resp = jsonify(response)
+    resp = make_response(jsonify(response))
     resp.set_cookie('attempts', str(attempts))
     resp.set_cookie('correct_guesses', str(correct_guesses))
 
@@ -73,21 +68,24 @@ def guess():
         resp.set_cookie('number', str(random.randint(1, 100)))
         resp.set_cookie('attempts', '0')
 
+    if 'new_high_score' in response:
+        save_high_score(resp, response['new_high_score'])
+
     return resp
 
 @app.before_request
 def before_request():
     if not request.cookies.get('number'):
-        resp = app.make_response(render_template('index.html'))
+        resp = make_response(render_template('index.html'))
         resp.set_cookie('number', str(random.randint(1, 100)))
         resp.set_cookie('attempts', '0')
         return resp
 
 @app.route('/reset_highscore', methods=['POST'])
 def reset_high_score():
-    if os.path.exists("highscore.txt"):
-        os.remove("highscore.txt")
-    return jsonify({'message': 'High score has been reset!'})
+    resp = make_response(jsonify({'message': 'High score has been reset!'}))
+    resp.set_cookie('high_score', '0')
+    return resp
 
 if __name__ == '__main__':
     app.run(debug=True)
